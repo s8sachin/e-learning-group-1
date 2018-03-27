8fea1a36f49c4eb96f089ec491569b323f49b9db
deepikacs <dshivalingegowda@qwinix.io>
2018-03-27 18:44:25 +0530

    changes2

--- js/course1.js
+++ js/course1.js
@@ -107,4 +107,38 @@ $(document).ready(function(){
     
       $("#t9").toggle();
       });
+//       $('.like-btn').on('click', function() {
+//       $(this).toggleClass('is-active');
+//       });
+//       $('.minus-btn').on('click', function(e) {
+//     e.preventDefault();
+//     var $this = $(this);
+//     var $input = $this.closest('div').find('input');
+//     var value = parseInt($input.val());
+ 
+//     if (value &amp;gt; 1) {
+//         value = value - 1;
+//     } else {
+//         value = 0;
+//     }
+ 
+//   $input.val(value);
+ 
+// });
+ 
+// $('.plus-btn').on('click', function(e) {
+//     e.preventDefault();
+//     var $this = $(this);
+//     var $input = $this.closest('div').find('input');
+//     var value = parseInt($input.val());
+ 
+//     if (value &amp;lt; 100) {
+//         value = value + 1;
+//     } else {
+//         value =100;
+//     }
+ 
+//     $input.val(value);
+// });
+
 });
