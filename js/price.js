8fc9a86b5eac16ae8f97e513475dbf3a5df9b4cf
22brunda <bsrinivas@qwinix.io>
2018-03-27 16:32:12 +0530

    part-3

--- js/price.js
+++ js/price.js
@@ -1,31 +1,100 @@
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+    $("#myBtn1").click(function(){
+        $("#myModal1").modal();
+    });
+    $("form[name='registration']").validate({
+
+      rules: {
+
+        firstname: "required",
+        lastname: "required",
+        email: {
+          required: true,
+
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 8
+        },
+        cnfpassword: {
+          required: true,
+          minlength: 8
+        }
+
+      },
+      messages: {
+        firstname: "Please enter your firstname",
+        lastname: "Please enter your lastname",
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 8 characters long"
+        },
+        cnfpassword: {
+          required: "Please confirm a password",
+          minlength: "Your password must be at least 8 characters long"
+        },
+        email: "Please enter a valid email address"
+      },
+
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+// function checkPasswordMatch() {
+//     debugger;
+//     var password = $("#password").val();
+//     var confirmPassword = $("#cnpassword").val();
+//         debugger;
+//     if (password != confirmPassword){
+//         $("#divCheckPasswordMatch").html("Passwords do not match!");
+//       }
+//     else{
+//         $("#divCheckPasswordMatch").html("Passwords match.");
+//       }
+// }
+$("#cnpassword").keyup(function() {
+  var password = $("#password").val();
+  $("#divCheckPasswordMatch").html(password == $(this).val() ? "Passwords match." : "Passwords do not match!");
+});
+
+(function ($) {
+  $.toggleShowPassword = function (options) {
+    var settings = $.extend({
+      field: "#password",
+      control: "#toggle_show_password",
+    }, options);
+
+    var control = $(settings.control);
+    var field = $(settings.field)
+
+    control.bind('click', function () {
+      if (control.is(':checked')) {
+        field.attr('type', 'text');
+      } else {
+        field.attr('type', 'password');
+      }
+    })
+  };
+}(jQuery));
+
+//Here how to call above plugin from everywhere in your application document body
+$.toggleShowPassword({
+  field: '#password',
+  control: '#test2'
+});
+
+
+});
+
 function mouseOver() {
   document.getElementById("para").style.color = "red";
 }
 
 function mouseOut() {
   document.getElementById("para").style.color = "black";
-}
-
-var tbl = document.getElementById("tblMain");
-
-if (tbl != null) {
-
-  if (tbl.rows[0] != null) {
-
-    tbl.rows[0].style.backgroundColor = "#365890";
-
-    tbl.rows[0].style.color = "#FFFFFF";
-
-  }
-
-  for (var i = 1; i < tbl.rows.length; i++) {
-
-    tbl.rows[i].style.cursor = "pointer";
-
-    tbl.rows[i].onmousemove = function () { this.style.backgroundColor = "#FF2080"; this.style.color = "#FFFFFF"; };
-
-    tbl.rows[i].onmouseout = function () { this.style.backgroundColor = ""; this.style.color = ""; };
-
-  }
-
 }
