e9a7e9120da72abcd532f976aba0db036b882d46
darshan405 <dshivakumar@qwinix.io>
2018-03-28 13:22:53 +0530

    re-indent

--- js/price.js
+++ js/price.js
@@ -1,113 +1,84 @@
 $(document).ready(function(){
-    $(".myBtn").click(function(){
-      debugger
-        $("#myModal").modal();
-    });
-  
-   //  $("#myBtn1").click(function(){
-   //    debugger
-   //      $("#myModal1").modal();
-   //  });
+  $(".myBtn").click(function(){
+    $("#myModal").modal();
+  });
+  $(".registration").validate({
 
-   //  $("#myBtn2").click(function(){
-   //    debugger
-   //      $("#myModal2").modal();
-   //  });
+    rules: {
 
-   // $("#myBtn3").click(function(){
-   //    debugger
-   //      $("#myModal3").modal();
-   //  });
-    $(".registration").validate({
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
 
-      rules: {
-
-        firstname: "required",
-        lastname: "required",
-        email: {
-          required: true,
-
-          email: true
-        },
-        password: {
-          required: true,
-          minlength: 8
-        },
-        cnfpassword: {
-          required: true,
-         minlength: 8
-        }
-
+        email: true
       },
-      messages: {
-        firstname: "Please enter your firstname",
-        lastname: "Please enter your lastname",
-        password: {
-          required: "Please provide a password",
-          minlength: "Your password must be at least 8 characters long"
-        },
-       cnfpassword: {
-         required: "Please confirm a password",
-          inlength: "Your password must be at least 8 characters long"
-        },
-        email: "Please enter a valid email address"
+      password: {
+        required: true,
+        minlength: 8
       },
-
-      submitHandler: function(form) {
-        form.submit();
+      cnfpassword: {
+        required: true,
+        minlength: 8
       }
-    });
-
-function checkPasswordMatch() {
-    debugger;
+    },
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 8 characters long"
+      },
+      cnfpassword: {
+       required: "Please confirm a password",
+       inlength: "Your password must be at least 8 characters long"
+     },
+     email: "Please enter a valid email address"
+   },
+   submitHandler: function(form) {
+    form.submit();
+  }
+});
+  function checkPasswordMatch() {
     var password = $("#password").val();
     var confirmPassword = $("#cnpassword").val();
-        debugger;
     if (password != confirmPassword){
-        $("#divCheckPasswordMatch").html("Passwords do not match!");
-      }
+      $("#divCheckPasswordMatch").html("Passwords do not match!");
+    }
     else{
-        $("#divCheckPasswordMatch").html("Passwords match.");
-      }
- }
-$("#cnpassword").keyup(function() {
-  var password = $("#password").val();
-  $("#divCheckPasswordMatch").html(password == $(this).val() ? "Passwords match." : "Passwords do not match!");
+      $("#divCheckPasswordMatch").html("Passwords match.");
+    }
+  }
+  $("#cnpassword").keyup(function() {
+    var password = $("#password").val();
+    $("#divCheckPasswordMatch").html(password == $(this).val() ? "Passwords match." : "Passwords do not match!");
+  });
+  (function ($) {
+    $.toggleShowPassword = function (options) {
+      var settings = $.extend({
+        field: "#password",
+        control: "#toggle_show_password",
+      }, options);
+      var control = $(settings.control);
+      var field = $(settings.field)
+
+      control.bind('click', function () {
+        if (control.is(':checked')) {
+          field.attr('type', 'text');
+        } else {
+          field.attr('type', 'password');
+        }
+      })
+    };
+  });
+  $.toggleShowPassword({
+    field: '#password',
+    control: '#test2'
+  });
 });
-
-(function ($) {
-  $.toggleShowPassword = function (options) {
-    var settings = $.extend({
-      field: "#password",
-      control: "#toggle_show_password",
-    }, options);
-
-    var control = $(settings.control);
-    var field = $(settings.field)
-
-    control.bind('click', function () {
-      if (control.is(':checked')) {
-        field.attr('type', 'text');
-      } else {
-        field.attr('type', 'password');
-      }
-    })
-  };
-}(jQuery));
-
-//Here how to call above plugin from everywhere in your application document body
-$.toggleShowPassword({
-  field: '#password',
-  control: '#test2'
-});
-
-});
-
-
 function mouseOver() {
   document.getElementById("para").style.color = "red";
 }
-
 function mouseOut() {
   document.getElementById("para").style.color = "black";
 }
