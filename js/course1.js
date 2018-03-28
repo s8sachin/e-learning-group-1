409e1e39af8f033cfa5ae1daa74a8caaf4282cc6
deepikacs <dshivalingegowda@qwinix.io>
2018-03-28 13:10:23 +0530

    changes8

--- js/course1.js
+++ js/course1.js
@@ -1,6 +1,4 @@
-
 filterSelection("all");
-
 function filterSelection(c) {
  var x, i;
   x = document.getElementsByClassName("column");
@@ -10,7 +8,6 @@ function filterSelection(c) {
    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
  }
 }
-
 function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -19,7 +16,6 @@ function AddClass(element, name) {
     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 }
-
 function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -45,75 +41,58 @@ for (var i = 0; i < btns.length; i++) {
 }
 $(document).ready(function(){
   filterSelection('all');
-
-    
-    $("#t1").hide();
-    
-     $("#im1").hover(function(){
-      
-      $("#t1").toggle();
-
-    });
-
-     $("#t2").hide();
-      $("#im2").hover(function(){
-      
-      $("#t2").toggle();
-      });
-      $("#t3").hide();
-      $("#im3").hover(function(){
-    
-      $("#t3").toggle();
-      });
-      $("#t4").hide();
-      $("#im4").hover(function(){
-    
-      $("#t4").toggle();
-      });
-      $("#t5").hide();
-      $("#im5").hover(function(){
-    
-      $("#t5").toggle();
-      });
-      $("#t6").hide();
-      $("#im6").hover(function(){
-    
-      $("#t6").toggle();
-      });
-      $("#t7").hide();
-      $("#im7").hover(function(){
-    
-      $("#t7").toggle();
-      });
-      $("#t8").hide();
-      $("#im8").hover(function(){
-    
-      $("#t8").toggle();
-      });
-      $("#t9").hide();
-      $("#im9").hover(function(){
-    
-      $("#t9").toggle();
-      });
-      $("#product").hide();
-      $(".plus").click(function(){
-
-
-  var a=$("#product").val();
-  a++;
-  $("#cart").html(a++);
-  $("#product").val(a);
-
-});
+  $("#t1").hide();
+  $("#im1").hover(function(){
+    $("#t1").toggle();
+  });
+  $("#t2").hide();
+  $("#im2").hover(function(){
+    $("#t2").toggle();
+  });
+  $("#t3").hide();
+  $("#im3").hover(function(){
+    $("#t3").toggle();
+  });
+  $("#t4").hide();
+  $("#im4").hover(function(){
+    $("#t4").toggle();
+  });
+  $("#t5").hide();
+  $("#im5").hover(function(){
+    $("#t5").toggle();
+  });
+  $("#t6").hide();
+  $("#im6").hover(function(){
+    $("#t6").toggle();
+  });
+  $("#t7").hide();
+  $("#im7").hover(function(){
+    $("#t7").toggle();
+  });
+  $("#t8").hide();
+  $("#im8").hover(function(){
+    $("#t8").toggle();
+  });
+  $("#t9").hide();
+  $("#im9").hover(function(){
+    $("#t9").toggle();
+  });
+  
+  $("#product").hide();
+  $(".plus").click(function(){
+    var a=$("#product").val();
+    a++;
+    $("#cart").html(a++);
+    $("#product").val(a);
+  });
   $(".minus").click(function(){
-   
-  var b=$("#product").val();
-  if(b<=0)
-  {
-    return false;
-  }
-  b--;
-  $("#cart").html(b--);
-  $("#product").val(b);
-});
+    var b=$("#product").val();
+    if(b<=0)
+    {
+      return false;
+    }
+    b--;
+    $("#cart").html(b--);
+    $("#product").val(b);
+  });
 });
